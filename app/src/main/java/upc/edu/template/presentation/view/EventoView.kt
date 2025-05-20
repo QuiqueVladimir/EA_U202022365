683bcd46c33ebc550a842db96c6b8765da9125c5
QuiqueVladimir <89040472+QuiqueVladimir@users.noreply.github.com>
2025-05-20 15:28:03 -0500

    feat: addsdsd

--- app/src/main/java/upc/edu/template/presentation/view/EventoView.kt
+++ app/src/main/java/upc/edu/template/presentation/view/EventoView.kt
@@ -1,26 +1,23 @@
 package upc.edu.template.presentation.view
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 
@@ -31,11 +28,9 @@ import upc.edu.template.presentation.viewmodel.EventosViewModel
 fun EventoView(viewModel: EventosViewModel, OnEventoClick: (Int) -> Unit) {
     val eventos = viewModel.evento.collectAsState()
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        LazyColumn {
-            items(eventos.value) { evento ->
-                EventoListItemView(evento, OnEventoClick)
-            }
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(eventos.value) { evento ->
+            EventoListItemView(evento, OnEventoClick)
         }
     }
 }
@@ -48,13 +43,39 @@ fun EventoListItemView(
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp),
-        onClick = { onEventoClick(evento.id) }
+            .padding(8.dp)
+            .clickable { onEventoClick(evento.id) },
+        shape = RoundedCornerShape(8.dp),
     ) {
-        Column(modifier = Modifier.padding(8.dp)) {
-            Text(evento.title, fontWeight = Bold)
-            Text(evento.date)
-            Text(evento.location)
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            AsyncImage(
+                model = evento.image,
+                contentDescription = "Imagen del evento",
+                modifier = Modifier
+                    .padding(end = 8.dp),
+                contentScale = ContentScale.Crop
+            )
+
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = evento.title,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
+                Text(
+                    text = evento.date,
+                    color = Color.Gray
+                )
+                Text(
+                    text = evento.location,
+                    color = Color.Gray
+                )
+            }
         }
     }
 }
